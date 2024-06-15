ee2b504e5b205c7dd9f9fc2cf463d2fa8ec0514e
yashwanthyadav1901 <yashwanthyadav481@gmail.com>
2024-06-15 21:12:27 +0530

    basic setup finished

---
+++ redux-query/src/features/todos/TodoList.jsx
@@ -0,0 +1,44 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faTrash, faUpload } from "@fortawesome/free-solid-svg-icons";
+import { FaCloudUploadAlt } from "react-icons/fa";
+import { useState } from "react";
+
+const TodoList = () => {
+  const [newTodo, setNewTodo] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //addTodo
+    setNewTodo("");
+  };
+
+  const newItemSection = (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="new-todo">Enter a new todo item</label>
+      <div className="new-todo">
+        <input
+          type="text"
+          id="new-todo"
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+          placeholder="Enter new todo"
+        />
+      </div>
+      <button className="submit">
+        <FaCloudUploadAlt />
+      </button>
+    </form>
+  );
+
+  let content;
+  // Define conditional content
+
+  return (
+    <main>
+      <h1>Todo List</h1>
+      {newItemSection}
+      {content}
+    </main>
+  );
+};
+export default TodoList;
